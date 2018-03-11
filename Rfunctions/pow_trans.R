ac40df3d0c91616c2f9b85bf460567eb1b4ac347
mcanouil <mickael.canouil@cnrs.fr>
2018-03-11 21:10:26 +0100

    Fix min max

--- Rfunctions/pow_trans.R
+++ Rfunctions/pow_trans.R
@@ -1,12 +1,22 @@
-pow_trans <- function () {
-    negpow_breaks <- function (n = 5) {
-        function (x, y = 0.5) {
-            rng <- range(x, na.rm = TRUE)^y
-            min <- ceiling(rng[2])
-            max <- floor(rng[1])
-            by <- floor((max - min)/n) + 1
-            seq(min, max, by = by)^(1/y)
-        }
+pow_trans <- function(power) {
+  negpow_breaks <- function(n = 5) {
+    function(x, y = power) {
+      rng <- range(x, na.rm = TRUE) ^ y
+      max <- ceiling(rng[2])
+      min <- floor(rng[1])
+      by <- floor((max - min) / n) + 1
+      seq(min, max, by = by) ^ (1 / y)
     }
-    trans_new(name = "pow", transform = function (x, y = 0.5) {x^y}, inverse = function (x, y = 0.5) {x^(1/y)}, breaks = negpow_breaks(), domain = c(0, Inf))
+  }
+  trans_new(
+    name = "pow", 
+    transform = function(x, y = power) {
+      x ^ y
+    }, 
+    inverse = function(x, y = power) {
+      x ^ (1 / y)
+    }, 
+    breaks = negpow_breaks(), 
+    domain = c(-Inf, Inf)
+  )
 }

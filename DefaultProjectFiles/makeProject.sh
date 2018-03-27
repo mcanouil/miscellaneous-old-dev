1e224946482aa8b461a0f0af837742cdf49edcd6
mcanouil <mickael.canouil@cnrs.fr>
2018-03-27 18:05:49 +0200

    update makeProject to include new bash script

--- DefaultProjectFiles/makeProject.sh
+++ DefaultProjectFiles/makeProject.sh
@@ -34,14 +34,16 @@ ln -s /disks/DATA/$projectName/ /disks/PROJECT/$projectName/Data
 
 cat /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/defaultScript.R | sed -e "s/PRJCT/$projectName/g" > "/disks/PROJECT/$projectName/Scripts/00-${projectName}.R"
 cat /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/defaultScript.Rmd | sed -e "s/PRJCT/$projectName/g" > "/disks/PROJECT/$projectName/Scripts/00-${projectName}.Rmd"
+cat /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/defaultScript.sh | sed -e "s/PRJCT/$projectName/g" > "/disks/PROJECT/$projectName/Scripts/00-${projectName}.sh"
 
 cp /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/gitignore.txt /disks/PROJECT/$projectName/.gitignore
 
 cd /disks/PROJECT/$projectName/
 git init
-git add .
-git commit -m 'Initialise'
 git remote add origin git@gitlab.egid.local:BioStat/$projectName.git
+git add --all
+git commit -m 'Initial commit'
+git push -u origin master
 
 cd $currentdir
 unset currentdir

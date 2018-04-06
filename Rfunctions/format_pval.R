7d618556f50b667e4d707ce50b1a8cd76c124888
mcanouil <mickael.canouil@cnrs.fr>
2018-04-06 18:28:12 +0200

    add option to format as "math" or with "e"

--- Rfunctions/format_pval.R
+++ Rfunctions/format_pval.R
@@ -1,7 +1,8 @@
-format_pval <- function (x, thresh = 10^-2, digits = 3, eps = 1e-50) {
-    ifelse(
-        x>=thresh, 
-        Hmisc::format.pval(x, digits = digits, eps = eps, nsmall = digits), 
-        base::format.pval(x, digits = digits, eps = eps, scientific = TRUE, nsmall = digits)
-    )
+format_pval <- function (x, thresh = 10^-2, digits = 3, eps = 1e-50, math_format = FALSE) {
+  pout <- ifelse(
+    x>=thresh, 
+    Hmisc::format.pval(x, digits = digits, eps = eps, nsmall = digits), 
+    base::format.pval(x, digits = digits, eps = eps, scientific = TRUE, nsmall = digits)
+  )
+  ifelse(math_format, gsub("e", " %*% 10^", pout), pout)
 }

16bd69efe0112dc1e6c38ef766948e8c67bb76ff
mcanouil <8896044+mcanouil@users.noreply.github.com>
2018-04-11 14:09:16 +0200

    Fix ifelse statement
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- Rfunctions/format_pval.R
+++ Rfunctions/format_pval.R
@@ -4,5 +4,9 @@ format_pval <- function (x, thresh = 10^-2, digits = 3, eps = 1e-50, math_format
     Hmisc::format.pval(x, digits = digits, eps = eps, nsmall = digits), 
     base::format.pval(x, digits = digits, eps = eps, scientific = TRUE, nsmall = digits)
   )
-  ifelse(math_format, gsub("e", " %*% 10^", pout), pout)
+  if (math_format) {
+    return(gsub("e", " %*% 10^", pout))
+  } else {
+    return(pout)
+  }
 }

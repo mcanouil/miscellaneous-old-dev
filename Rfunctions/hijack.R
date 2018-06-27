69258e435f75b1476a580a2793e613f00c02ad33
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-06-27 18:22:17 +0200

    from theme_black.R

---
+++ Rfunctions/hijack.R
@@ -0,0 +1,22 @@
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param FUN PARAM_DESCRIPTION
+#' @param ... PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname hijack
+#' @export 
+hijack <- function (FUN, ...) {
+    .FUN <- FUN
+    args <- list(...)
+    invisible(lapply(seq_along(args), function(i) {
+        formals(.FUN)[[names(args)[i]]] <<- args[[i]]
+    }))
+    .FUN
+}

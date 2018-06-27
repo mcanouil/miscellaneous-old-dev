5dbfd850adeb627e167803cd92efce8164851a25
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-06-27 15:17:40 +0200

    restyle code

--- Rfunctions/pval_trans.R
+++ Rfunctions/pval_trans.R
@@ -1,24 +1,29 @@
 pval_trans <- function () {
-    neglog10_breaks <- function (n = 5) {
-        function (x) {
-            rng <- -log(range(x, na.rm = TRUE), base = 10)
-            min <- ceiling(rng[2])
-            max <- floor(rng[1])
-            if (max == min) {
-                return(10^-min)
-            } else {}
-            by <- floor((max - min)/n) + 1
-            10^-seq(min, max, by = by)
-        }
+  neglog10_breaks <- function (n = 5) {
+    function (x) {
+      rng <- -log(range(x, na.rm = TRUE), base = 10)
+      min <- ceiling(rng[2])
+      max <- floor(rng[1])
+      if (max == min) {
+        return(10^-min)
+      } else {
+        by <- floor((max - min)/n) + 1
+        return(10^-seq(min, max, by = by))
+      }
     }
-    trans_new(
-        name = "pval", 
-        transform = function (x) {-log(x, 10)}, 
-        inverse = function (x) {10^-x}, 
-        breaks = neglog10_breaks(), 
-        domain = c(1e-100, Inf),
-        format = function(x) {
-            parse(text = gsub("e", " %*% 10^", scientific_format()(x)))
-        }
-    )
+  }
+  trans_new(
+    name = "pval", 
+    transform = function (x) {-log(x, 10)}, 
+    inverse = function (x) {10^-x}, 
+    breaks = neglog10_breaks(), 
+    domain = c(1e-100, Inf),
+    format = function(x) {
+      parse(
+        text = scientific_format()(x) %>% 
+          gsub("1e+00", "1", ., fixed = TRUE) %>% 
+          gsub("e", " %*% 10^", .)
+      )
+    }
+  )
 }

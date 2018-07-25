3417933b7ff23b3bae93ab15caa6725bf2506ea4
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2018-07-25 15:59:21 +0200

    rename the function; add package prefix
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

---
+++ Rfunctions/pretty_kable.R
@@ -0,0 +1,49 @@
+pretty_kable <- function (
+    data, 
+    font_size = 12, 
+    format_args = list(scientific = -1, digits = 3, big.mark = ","), 
+    col.names = NA,
+    pval_cols = NULL,
+    ...
+) {
+	format_pval <- function (x, thresh = 10^-2, digits = 3, eps = 1e-50) {
+		ifelse(
+			x>=thresh, 
+			Hmisc::format.pval(x, digits = digits, eps = eps, nsmall = digits), 
+			base::format.pval(x, digits = digits, eps = eps, scientific = TRUE, nsmall = digits)
+		)
+	}
+  if (!is.null(pval_cols)) {
+    data[, pval_cols] <- format_pval(
+      x = data[, pval_cols], 
+      digits = format_args$digits
+    )
+  }
+  colnames(data) <- Hmisc::capitalize(colnames(data))
+  if (knitr:::is_latex_output()) {
+    options(knitr.table.format = "latex")
+    output <- knitr::kable(x = data, booktabs = TRUE, format.args = format_args, col.names = col_names, ...)
+    kableExtra::kable_styling(
+      kable_input = output,
+      latex_options = c("striped", "hold_position"),
+      full_width = FALSE,
+      position = "center",
+      font_size = font_size
+    )
+  } else {
+    options(knitr.table.format = "html")
+    output <- knitr::kable(x = data, format.args = format_args, col.names = col_names, ...)
+    kableExtra::kable_styling(
+      kable_input = output,
+      bootstrap_options = c("striped", "hover", "condensed", "responsive"),
+      full_width = TRUE,
+      position = "center",
+      font_size = font_size
+    )
+  }
+}
+
+mykable <- function(...) {
+  warning("'mykable()' is deprecated, please use 'pretty_kable()' instead!")
+  pretty_kable(...)
+}

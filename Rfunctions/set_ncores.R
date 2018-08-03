83cca3d93fa1982bf4b7c6e90be689df34744a99
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-08-03 13:26:26 +0200

    add parameters such as channel

--- Rfunctions/set_ncores.R
+++ Rfunctions/set_ncores.R
@@ -1,10 +1,15 @@
-set_ncores <- function(n_cores = parallel::detectCores(), hooks = NULL) {
+set_ncores <- function(n_cores = parallel::detectCores(), channel = NULL, hooks = NULL, auth_token = NULL, user_id = NULL) {
   if (is.null(hooks)) {
     stop('"hooks" is missing and must be provided!')
   }
+  # hooks <- 'http://chat.egid.local/api/v1/chat.postMessage'
   cmd <- paste(
-    'curl -X POST -H "Content-Type: application/json"',
-    '--data \'{"text":"', 
+    'curl',
+    '-X POST',
+    '-H "Content-Type: application/json"',
+    # '-H "X-Auth-Token: ', auth_token, '"',
+    # '-H "X-User-Id: ', user_id, '"',
+    '--data \'{"channel": "', channel, '", "text":"', 
     paste0(n_cores, ' cores are currently being used by ', Sys.getenv("LOGNAME"), '!'),
     '"}\'',
     hooks,

13fecc36a984cca2b21e66e3fb9e46ccd3ca157f
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2018-08-08 13:23:35 +0200

    change "<<<" by "echo"
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- Rfunctions/mail.R
+++ Rfunctions/mail.R
@@ -16,35 +16,35 @@
 #' @rdname mail
 #' @export 
 mail <- function (from, subject, to, body, attachment) {
-    if (missing(subject)) {
-        eMailSubject <- '-s "(no subject)"'
+  if (missing(subject)) {
+    eMailSubject <- '-s "(no subject)"'
+  } else {
+    eMailSubject <- paste0('-s "', subject, '"')
+  }
+  if (missing(to)) {
+    stop('"to" is missing!')
+  }
+  if (missing(body)) {
+    eMailBody <- 'echo "" |'
+  } else {
+    eMailBody <-  paste0('echo "', body, '" |')
+  }
+  if (missing(attachment)) {
+    eMailJoint <- ""
+  } else {
+    toAttach <- sapply(seq_along(attachment), function(attach) {
+      return(!system(paste("test -f", attachment[attach])))
+    })
+    if (any(toAttach)) {
+      eMailJoint <- paste(paste0("-a", attachment[toAttach]), collapse = " ")
     } else {
-        eMailSubject <- paste0('-s "', subject, '"')
+      eMailJoint <- ""
     }
-    if (missing(to)) {
-        stop('"to" is missing!')
-    } else {}
-    if (missing(body)) {
-        eMailBody <- '<<< ""'
-    } else {
-        eMailBody <-  paste0('<<< "', body, '"')
-    }
-    if (missing(attachment)) {
-        eMailJoint <- ""
-    } else {
-        toAttach <- sapply(seq_along(attachment), function(attach) {
-            return(!system(paste("test -f", attachment[attach])))
-        })
-        if (any(toAttach)) {
-            eMailJoint <- paste(paste0("-a", attachment[toAttach]), collapse = " ")
-        } else {
-            eMailJoint <- ""
-        }
-    }
-    if (missing(from)) {
-        eMailFrom <- ""
-    } else {
-        eMailFrom <- paste("-r", from)
-    }
-    return(system(paste('mail', eMailFrom, eMailSubject, eMailJoint, to, eMailBody)))
+  }
+  if (missing(from)) {
+    eMailFrom <- ""
+  } else {
+    eMailFrom <- paste("-r", from)
+  }
+  return(system(paste(eMailBody, 'mail', eMailFrom, eMailSubject, eMailJoint, to)))
 }

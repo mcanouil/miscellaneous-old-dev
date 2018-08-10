a2b1461c295ee8a8b8c667796378909b3b7d10e4
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2018-08-10 15:28:51 +0200

    major update to the script; too much changes
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- Rfunctions/pca_report.R
+++ Rfunctions/pca_report.R
@@ -1,71 +1,138 @@
 pca_report <- function(
-                       data,
-                       design,
-                       techvars,
-                       n.comp = 5,
-                       max.comp = ncol(data) - 1 - n.comp,
-                       outliers.comp = c(1, 2),
-                       outliers.thresh = 3,
-                       title.level = 2,
-                       theme_dark = FALSE) {
+  data,
+  design,
+  id_var = "Sample_ID",
+  technical_vars,
+  n_comp = 5,
+  outliers_component = NULL,
+  outliers_threshold = 3,
+  title_level = 2
+) {
   require(flashpcaR)
   require(scales)
   require(tidyverse)
+  
+  if (!is(design, "data.frame")) {
+    design <- as.data.frame(design)
+  }
+  if (!is(design[, id_var], "character")) {
+    design[, id_var] <- as.character(design[, id_var])
+  }
 
-  pca.res <- flashpca(
-    X = as.matrix(data),
-    method = "eigen",
-    transpose = TRUE,
+  pca_res <- flashpca(
+    X = t(as.matrix(data)[, seq(8)]),
     stand = "sd",
-    ndim = n.comp,
-    nextra = max.comp,
-    maxiter = 100
+    ndim = n_comp
   )
-  pca.dfxy <- pca.res %>%
+  
+  pca_dfxy <- pca_res %>%
     `[[`("projection") %>%
     as.data.frame() %>%
-    `rownames<-`(colnames(data)) %>%
     `colnames<-`(paste0("PC", seq_len(ncol(.)))) %>%
-    merge(x = design, y = ., by = "row.names", all.y = TRUE) %>%
-    column_to_rownames(var = "Row.names")
+    mutate(Sample_ID = as.character(colnames(data))) %>% 
+    left_join(x = design, y = ., by = "Sample_ID")
 
-  pca.outliers <- pca.dfxy[, paste0("PC", outliers.comp), drop = FALSE] %>%
-    `^`(2) %>%
-    rowSums() %>%
-    sqrt() %>%
-    data.frame(EuclideanDistance = .) %>%
-    rownames_to_column(var = "SampleID") %>%
-    mutate(
-      BadSamplesLogical = EuclideanDistance <= (median(EuclideanDistance) - outliers.thresh * IQR(EuclideanDistance)) |
-        EuclideanDistance >= (median(EuclideanDistance) + outliers.thresh * IQR(EuclideanDistance)),
-      BadSamples = factor(ifelse(BadSamplesLogical, "BAD", "GOOD"), levels = c("BAD", "GOOD"))
-    ) %>%
-    column_to_rownames(var = "SampleID")
 
-  pca.dfxy <- merge(
-    x = pca.dfxy,
-    y = pca.outliers,
-    by = "row.names"
+  cat(paste0("\n", paste(rep("#", title_level), collapse = ""), " PCA inertia contribution {-}\n"))
+  p <- data_frame(
+    y = (pca_res$values / sum(pca_res$values)), 
+    x = sprintf("PC%02d", seq_along(pca_res$values))
   ) %>%
-    column_to_rownames(var = "Row.names")
-
-
-  cat(paste0("\n", paste(rep("#", title.level), collapse = ""), " PCA inertia contribution {-}\n"))
-  p <- data.frame(y = (pca.res$x$values / sum(pca.res$x$values)), x = seq_along(pca.res$x$values)) %>%
     mutate(cumsum = cumsum(y)) %>%
     ggplot(aes(x = x, y = y)) +
-    geom_bar(stat = "identity", width = 1, colour = "white", fill = viridis_pal(option = "viridis", begin = 0.25, end = 0.25)(1)) +
+    geom_bar(stat = "identity", width = 1, colour = "white", fill = "#3B528BFF") +
     scale_y_continuous(labels = percent) +
-    scale_x_continuous(breaks = seq_len(n.comp), limits = c(0, 11), expand = c(0, 0)) +
     labs(y = "Inertia", x = "PCA Components")
   print(p)
   cat("\n")
 
-  cat(paste0("\n", paste(rep("#", title.level), collapse = ""), " PCA  factorial planes {-, .tabset}\n"))
-  for (ivar in techvars) {
-    cat(paste0("\n", paste(rep("#", title.level + 1), collapse = ""), " ", ivar, " {-}\n"))
-    p <- do.call("rbind", apply(t(combn(paste0("PC", seq_len(n.comp)), 2)), 1, function(icoord) {
-      tmp <- pca.dfxy[, c(ivar, icoord)]
+  cat(paste0("\n", paste(rep("#", title_level), collapse = ""), " PCA  factorial planes {- .tabset}\n"))
+  for (ivar in technical_vars) {
+    cat(paste0("\n", paste(rep("#", title_level + 1), collapse = ""), " ", ivar, " {-}\n"))
+    p <- do.call("rbind", apply(t(combn(paste0("PC", seq_len(n_comp)), 2)), 1, function(icoord) {
+      tmp <- pca_dfxy[, c(ivar, icoord)]
+      tmp[, ivar] <- as.factor(tmp[, ivar])
+      colnames(tmp)[-seq_len(ncol(tmp) - 2)] <- c("X", "Y")
+      tmp[, "X.PC"] <- icoord[1]
+      tmp[, "Y.PC"] <- icoord[2]
+      return(tmp)
+    })) %>%
+      ggplot(aes_string(x = "X", y = "Y", colour = ivar)) +
+      geom_hline(aes(yintercept = 0), colour = ifelse(theme_dark, "white", "black")) +
+      geom_vline(aes(xintercept = 0), colour = ifelse(theme_dark, "white", "black")) +
+      geom_point(shape = 4, size = 2) +
+      stat_ellipse(type = "norm") +
+      scale_colour_viridis_d() +
+      labs(x = NULL, y = NULL) +
+      facet_grid(Y.PC ~ X.PC, scales = "free") +
+      guides(colour = ifelse(length(unique(pca_dfxy[, ivar])) <= 12, "legend", "none"))
+    print(p)
+    cat("\n")
+  }
+
+  cat(paste0("\n", paste(rep("#", title_level), collapse = ""), " PCA association {-}\n"))
+  technical_vars <- names(which(
+    apply(
+      X = pca_dfxy[, technical_vars], 
+      MARGIN = 2, 
+      FUN = function(icol) {length(unique(icol))>1}
+    )
+  ))
+  p <- pca_dfxy %>%
+    (function(.data) {
+      lapply(seq_len(n_comp), function(i) {
+        form <- as.formula(paste0("PC", i, " ~ ", paste(paste0("factor(", technical_vars, ")"), collapse = " + ")))
+        lm(form, data = .data) %>% 
+          anova() %>% 
+          rownames_to_column(var = "term") %>% 
+          mutate(PC = i)
+      }) %>%
+        bind_rows()
+    }) %>%
+    filter(term != "Residuals") %>%
+    mutate(term = gsub("factor\\((.*)\\)", "\\1", term)) %>%
+    ggplot(aes(x = factor(PC), y = term, fill = `Pr(>F)`)) +
+    geom_tile(colour = "white") +
+    geom_text(aes(label = scientific(`Pr(>F)`, digits = 2)), colour = "white", size = 3) +
+    scale_fill_viridis_c(name = "P-Value", na.value = "grey85", limits = c(0, 0.1)) +
+    theme(panel.grid = element_blank()) +
+    scale_x_discrete(expand = c(0, 0)) +
+    scale_y_discrete(expand = c(0, 0)) +
+    labs(x = "PCA Components", y = NULL)
+  print(p)
+  cat("\n")
+
+
+  if (!is.null(outliers_component)) {
+    cat(paste0("\n", paste(rep("#", title_level), collapse = ""), " PCA Outliers {-}\n"))
+    
+    pca_outliers <- pca_dfxy[, paste0("PC", outliers_component), drop = FALSE] %>%
+      `^`(2) %>%
+      rowSums() %>%
+      sqrt() %>%
+      data_frame(EuclideanDistance = .) %>%
+      rownames_to_column(var = "SampleID") %>%
+      mutate(
+        BadSamplesLogical = 
+          EuclideanDistance <= 
+            (median(EuclideanDistance) - outliers_threshold * IQR(EuclideanDistance)) |
+          EuclideanDistance >= 
+            (median(EuclideanDistance) + outliers_threshold * IQR(EuclideanDistance)),
+        BadSamples = factor(ifelse(BadSamplesLogical, "BAD", "GOOD"), levels = c("BAD", "GOOD"))
+      ) %>%
+      column_to_rownames(var = "SampleID")
+  
+    pca_dfxy <- merge(
+      x = pca_dfxy,
+      y = pca_outliers,
+      by = "row.names"
+    ) %>%
+      column_to_rownames(var = "Row.names")
+    
+    ivar <- "BadSamples"
+    
+    p <- do.call("rbind", apply(t(combn(paste0("PC", seq_len(n_comp)), 2)), 1, function(icoord) {
+      tmp <- pca_dfxy[, c(ivar, icoord)]
       tmp[, ivar] <- as.factor(tmp[, ivar])
       colnames(tmp)[-seq_len(ncol(tmp) - 2)] <- c("X", "Y")
       tmp[, "X.PC"] <- icoord[1]
@@ -79,58 +146,14 @@ pca_report <- function(
       stat_ellipse(type = "norm") +
       scale_colour_viridis(discrete = TRUE) +
       labs(x = NULL, y = NULL) +
-      facet_grid(Y.PC ~ X.PC, scales = "free") +
-      guides(colour = ifelse(length(unique(pca.dfxy[, ivar])) <= 12, "legend", "none"))
+      facet_grid(Y.PC ~ X.PC, scales = "free")
     print(p)
     cat("\n")
+    
+    pca.dfxy %>%
+      select(-starts_with("PC")) %>%
+      filter(BadSamples == "BAD") %>%
+      return()
   }
-
-  cat(paste0("\n", paste(rep("#", title.level), collapse = ""), " PCA association {-}\n"))
-  p <- pca.dfxy %>%
-    (function(.data) {
-      lapply(seq_len(n.comp), function(i) {
-        form <- as.formula(paste0("PC", i, " ~ ", paste(paste0("factor(", techvars, ")"), collapse = " + ")))
-        lm(form, data = .data) %>% anova() %>% rownames_to_column(var = "term") %>% mutate(PC = i)
-      }) %>%
-        do.call("rbind", .)
-    }) %>%
-    filter(term != "Residuals") %>%
-    mutate(term = gsub("factor\\((.*)\\)", "\\1", term)) %>%
-    ggplot(aes(x = factor(PC), y = term, fill = `Pr(>F)`)) +
-    geom_tile(colour = "white") +
-    geom_text(aes(label = scientific(`Pr(>F)`, digits = 2)), colour = ifelse(!theme_dark, "white", "black"), size = 3) +
-    scale_fill_viridis(name = "P-Value", na.value = ifelse(theme_dark, "grey30", "grey85"), limits = c(0, 0.1)) +
-    theme(panel.grid = element_blank()) +
-    scale_x_discrete(expand = c(0, 0)) +
-    scale_y_discrete(expand = c(0, 0)) +
-    labs(x = "PCA Components", y = NULL)
-  print(p)
-  cat("\n")
-
-
-  cat(paste0("\n", paste(rep("#", title.level), collapse = ""), " PCA Outliers {-}\n"))
-  ivar <- "BadSamples"
-  p <- do.call("rbind", apply(t(combn(paste0("PC", seq_len(n.comp)), 2)), 1, function(icoord) {
-    tmp <- pca.dfxy[, c(ivar, icoord)]
-    tmp[, ivar] <- as.factor(tmp[, ivar])
-    colnames(tmp)[-seq_len(ncol(tmp) - 2)] <- c("X", "Y")
-    tmp[, "X.PC"] <- icoord[1]
-    tmp[, "Y.PC"] <- icoord[2]
-    return(tmp)
-  })) %>%
-    ggplot(aes_string(x = "X", y = "Y", colour = ivar)) +
-    geom_hline(aes(yintercept = 0), colour = ifelse(theme_dark, "white", "black")) +
-    geom_vline(aes(xintercept = 0), colour = ifelse(theme_dark, "white", "black")) +
-    geom_point(shape = 4, size = 2) +
-    stat_ellipse(type = "norm") +
-    scale_colour_viridis(discrete = TRUE) +
-    labs(x = NULL, y = NULL) +
-    facet_grid(Y.PC ~ X.PC, scales = "free")
-  print(p)
-  cat("\n")
-
-  pca.dfxy %>%
-    select(-starts_with("PC")) %>%
-    filter(BadSamples == "BAD") %>%
-    return()
+  return(invisible())
 }

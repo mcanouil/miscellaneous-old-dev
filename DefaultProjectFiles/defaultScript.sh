9496b8871ebdbe62f1aecabcb6fcf19f668e0554
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-08-17 09:26:41 +0200

    include gitignore as "echo"; small changes

--- DefaultProjectFiles/defaultScript.sh
+++ DefaultProjectFiles/defaultScript.sh
@@ -4,15 +4,14 @@
 R --slave --silent <<RSCRIPT
 dir.create(path = '/disks/PROJECT/Report/PRJCT', recursive = TRUE)
 rmarkdown::render(
-  input = '/disks/PROJECT/PRJCT/Scripts/00-PRJCT.Rmd', 
-  output_file = '00-PRJCT.html', 
+  input = '/disks/PROJECT/PRJCT/Scripts/00-main.Rmd', 
+  output_file = '00-main.html', 
   output_format = 'html_document', 
   output_dir = '/disks/PROJECT/Report/PRJCT',
   encoding = 'UTF-8'
 )
 RSCRIPT
 
-git -C /disks/PROJECT/Report/ commit \
-  -a PRJCT/00-PRJCT.html \
-  -m 'Update report PRJCT'
+git -C /disks/PROJECT/Report/ add PRJCT/00-main.html
+git -C /disks/PROJECT/Report/ commit -a PRJCT/00-main.html -m 'Update report PRJCT'
 
--- DefaultProjectFiles/makeProject.sh
+++ DefaultProjectFiles/makeProject.sh
@@ -5,7 +5,7 @@ mkdir /disks/PROJECT/$projectName
 
 currentdir=`pwd`
 
-echo "Engineer: Mickaël Canouil  \n" >> /disks/PROJECT/$projectName/README.md
+echo "Engineer: Mickaël Canouil  " >> /disks/PROJECT/$projectName/README.md
 
 printf "Version: 1.0
 
@@ -27,27 +27,57 @@ BuildType: Custom
 CustomScriptPath: Scripts/_build.sh
 
 QuitChildProcessesOnExit: Yes
-" >> /disks/PROJECT/$projectName/$projectName.Rproj
+" > /disks/PROJECT/$projectName/$projectName.Rproj
 
 for iFile in "Docs" "Report" "Scripts"
 do
   mkdir /disks/PROJECT/$projectName/$iFile
 done
 
-mkdir /disks/DATATMP/$projectName/
-ln -s /disks/DATATMP/$projectName/ /disks/PROJECT/$projectName/Data
+mkdir /disks/DATATMP/$projectName
+ln -s /disks/DATATMP/$projectName /disks/PROJECT/$projectName/Data
 
-cat /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/defaultScript.R | sed -e "s/PRJCT/$projectName/g" > "/disks/PROJECT/$projectName/Scripts/00-${projectName}.R"
-cat /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/defaultScript.Rmd | sed -e "s/PRJCT/$projectName/g" > "/disks/PROJECT/$projectName/Scripts/00-${projectName}.Rmd"
-cat /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/defaultScript.sh | sed -e "s/PRJCT/$projectName/g" > "/disks/PROJECT/$projectName/Scripts/_build.sh"
+cat /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/defaultScript.R \
+  | sed -e "s/PRJCT/$projectName/g" > "/disks/PROJECT/$projectName/Scripts/00-main.R"
+cat /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/defaultScript.Rmd \
+  | sed -e "s/PRJCT/$projectName/g" > "/disks/PROJECT/$projectName/Scripts/00-main.Rmd"
+cat /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/defaultScript.sh \
+  | sed -e "s/PRJCT/$projectName/g" > "/disks/PROJECT/$projectName/Scripts/_build.sh"
 
-cp /disks/PROJECT/Mickael/DEV/DefaultProjectFiles/gitignore.txt /disks/PROJECT/$projectName/.gitignore
+echo '/*
+
+**.Rproj
+**.Rproj.user
+**.Rhistory
+**.RData
+**.Rdata
+**.Ruserdata
+**.rdb
+**.rdx
+**.rds
+
+**.glo
+**.ist
+**.out
+**.nav
+**.log
+**.bbl
+**.blg
+**.aux
+**.toc
+**.snm
+**.html
+**.pdf
+
+!/Scripts/
+!README.md
+' > test /disks/PROJECT/$projectName/.gitignore
 
 cd /disks/PROJECT/$projectName/
 git init
 git remote add origin git@gitlab.egid.local:BioStat/$projectName.git
 git add --all
-git commit -m 'Initial commit'
+git commit -am 'Initial commit'
 # git push -u origin master
 
 cd $currentdir

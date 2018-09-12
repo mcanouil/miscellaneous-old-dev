507c6cb3510a0333079c8043cfe0bc95d6ad3f8c
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-09-12 14:39:16 +0200

    send_message to rocketchat

---
+++ Rfunctions/send_message.R
@@ -0,0 +1,23 @@
+send_message <- function(
+  message, 
+  tag = paste0('_', Sys.getenv("LOGNAME"), '@',  Sys.info()[["nodename"]], '_', ':'), 
+  hooks = NULL
+) {
+  cmd <- paste(
+    'curl',
+    '-X POST',
+    '-H "Content-Type: application/json"',
+    '--data \'{', 
+      '"text":"', tag, message, '"',
+    '}\'',
+    hooks,
+    '--silent'
+  )
+  res_cmd <- system(cmd, intern = TRUE)
+  
+  if (grepl("Bad Request", res_cmd) | !grepl("{\"success\":true}", res_cmd, fixed = TRUE)) {
+    stop("Request to send a message to chat failed!")
+  }
+  return(invisible())
+}
+

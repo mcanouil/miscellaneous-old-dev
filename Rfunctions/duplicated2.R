044d22a815f18c42c1e8da04dd4d0592abebe3a1
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2018-10-17 17:41:36 +0200

    reindent
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- Rfunctions/duplicated2.R
+++ Rfunctions/duplicated2.R
@@ -12,12 +12,12 @@
 #' @rdname duplicated2
 #' @export 
 duplicated2 <- function(x){
-    if (sum(dup <- duplicated(x)) == 0) {
-        return(dup)
-    }
-    if (class(x) %in% c("data.frame", "matrix")) {
-        return(duplicated(rbind(x[dup, ], x))[-(1:sum(dup))])
-    } else {
-        return(duplicated(c(x[dup], x))[-(1:sum(dup))])
-    }
+  if (sum(dup <- duplicated(x)) == 0) {
+    return(dup)
+  }
+  if (class(x) %in% c("data.frame", "matrix")) {
+    return(duplicated(rbind(x[dup, ], x))[-(1:sum(dup))])
+  } else {
+    return(duplicated(c(x[dup], x))[-(1:sum(dup))])
+  }
 }

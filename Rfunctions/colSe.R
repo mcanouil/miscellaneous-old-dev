dd564314668eb527ca6ab825716d23daae0b29f7
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-10-17 17:40:21 +0200

    reindent

--- Rfunctions/colSe.R
+++ Rfunctions/colSe.R
@@ -13,11 +13,11 @@
 #' @rdname colSe
 #' @export 
 colSe <- function (x, na.rm = TRUE) {
-    if (na.rm) {
-        n <- colSums(!is.na(x))
-    } else {
-        n <- nrow(x)
-    }
-    colVar <- colMeans(x*x, na.rm = na.rm) - (colMeans(x, na.rm = na.rm))^2
-    return(sqrt(colVar/n))
+  if (na.rm) {
+    n <- colSums(!is.na(x))
+  } else {
+    n <- nrow(x)
+  }
+  colVar <- colMeans(x*x, na.rm = na.rm) - (colMeans(x, na.rm = na.rm))^2
+  return(sqrt(colVar/n))
 }

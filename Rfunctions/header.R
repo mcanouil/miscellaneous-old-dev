b95d9f64b19873cbccf2f3e17a411d88d12f8038
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-10-17 17:45:45 +0200

    reindent

--- Rfunctions/header.R
+++ Rfunctions/header.R
@@ -13,11 +13,11 @@
 #' @rdname h
 #' @export 
 h <- header <- function(x, n = 10) {
-    if (is.matrix(x) | is.data.frame(x)) {
-        n1 <- min(nrow(x), n)
-        n2 <- min(ncol(x), n)
-        return(x[seq_len(n1), seq_len(n2)])
-    } else {
-        return(head(x, n))
-    }
+  if (is.matrix(x) | is.data.frame(x)) {
+    n1 <- min(nrow(x), n)
+    n2 <- min(ncol(x), n)
+    return(x[seq_len(n1), seq_len(n2)])
+  } else {
+    return(head(x, n))
+  }
 }

75c82c36245f619dd27fc1a5c8ac67e59e1bc131
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2018-10-17 17:40:10 +0200

    reindent
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- Rfunctions/colSd.R
+++ Rfunctions/colSd.R
@@ -13,11 +13,11 @@
 #' @rdname colSd
 #' @export 
 colSd <- function(x, na.rm = TRUE) {
-    if (na.rm) {
-        n <- colSums(!is.na(x))
-    } else {
-        n <- nrow(x)
-    }
-    colVar <- colMeans(x*x, na.rm = na.rm) - (colMeans(x, na.rm = na.rm))^2
-    return(sqrt(colVar * n/(n-1)))
+  if (na.rm) {
+    n <- colSums(!is.na(x))
+  } else {
+    n <- nrow(x)
+  }
+  colVar <- colMeans(x*x, na.rm = na.rm) - (colMeans(x, na.rm = na.rm))^2
+  return(sqrt(colVar * n/(n-1)))
 }

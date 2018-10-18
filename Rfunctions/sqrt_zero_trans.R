ea48f7623d1db708d11398aa1026ef74ed038d39
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-10-18 10:30:35 +0200

    add prefix

--- Rfunctions/sqrt_zero_trans.R
+++ Rfunctions/sqrt_zero_trans.R
@@ -1,5 +1,6 @@
 sqrt_zero_trans <- function() {
-  trans_new(
+  require(scales)
+  scales::trans_new(
     name = "sqrt_zero",
     transform = base::sqrt,
     inverse = function(x) ifelse(x==0, 0, x^2),

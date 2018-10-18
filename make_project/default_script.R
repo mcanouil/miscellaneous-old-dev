d69b949a0f5be80472f72d8c9e569b3a586bbe4c
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2018-10-18 13:23:59 +0200

    rename; update few details
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- make_project/default_script.R
+++ make_project/default_script.R
@@ -1,15 +1,20 @@
 # rm(list = ls())
 options(stringsAsFactors = FALSE)
 
-working_directory <- "/disks/DATATMP/PRJCT/"
+script_name <- ""
+project_directory <- gsub("Scripts", "", getwd())
+working_directory <- gsub("/PROJECT/", "/DATATMP/", project_directory)
+output_directory <- paste0(working_directory, "/", script_name)
+
+dir.create(paste0(output_directory, "/KnitrFiles/"), recursive = TRUE, showWarnings = FALSE, mode = '0777')
+
 setwd(working_directory)
-# ggsave(file = "Pictures/.png", plot = p, width = 6.3, height = 4.7, units = "in", dpi = 300)
 
 params <- list(
   n_cores = parallel::detectCores(),
-  theme_dark = TRUE,
   gg_fontsize = 12
 )
+
 
 ### Load packages
 library(tidyverse)
@@ -20,12 +25,6 @@ library(grid)
 
 tidyverse_conflicts()
 
-source("/disks/PROJECT/Rscripts/MC/format_pval.R")
-source("/disks/PROJECT/Rscripts/MC/ggheatmap.R")
-source("/disks/PROJECT/Rscripts/MC/ggmanhattan.R")
-source("/disks/PROJECT/Rscripts/MC/ggqqplot.R")
-
-source("/disks/PROJECT/Rscripts/MC/theme_black.R")
 
 ### Define n_cores
 n_cores <- params$n_cores
@@ -34,6 +33,7 @@ n_cores <- params$n_cores
 ### Define theme
 theme_dark <- params$theme_dark
 if (theme_dark) {
+  source("/disks/PROJECT/Rscripts/MC/theme_black.R")
   theme_set(theme_black(base_size = params$gg_fontsize))
 } else {
   theme_set(theme_light(base_size = params$gg_fontsize))

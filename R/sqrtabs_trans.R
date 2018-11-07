5b38fc040f88aae4969b184094d0fb70fc9beb08
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2018-11-07 14:25:11 +0100

    square root transformation for both positive and negative value
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

---
+++ R/sqrtabs_trans.R
@@ -0,0 +1,14 @@
+sqrtabs_trans <- function() {
+  f <- function(x) {sign(x) * sqrt(abs(x))}
+  g <- function(x) {sign(x) * x^2}
+  h <- function (n = 5) {
+    function (x) {
+      rng <- f(range(x, na.rm = TRUE))
+      max <- ceiling(rng[2])
+      min <- floor(rng[1])
+      by <- floor((max - min)/n)
+      return(g(seq(min, max, by = by)))
+    }
+  }
+  scales::trans_new(name = "sqrtabs", transform = f, inverse = g, breaks = h(), domain = c(-Inf, Inf))
+}

cd4c0ea19c8c8ba6c385c52dd80536b14852f520
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-02-15 16:52:24 +0100

    complete roxygen

--- R/gc_adjust.R
+++ R/gc_adjust.R
@@ -1,11 +1,9 @@
-#' Title
+#' gc_adjust
 #'
-#' @param stat 
+#' @param stat [numeric]
 #'
-#' @return
+#' @return numeric
 #' @export
-#'
-#' @examples
 gc_adjust <- function(stat) {
   gc_corr_factor <- median(stat^2) / qchisq(0.5, 1)
   quantile_stat <- (stat^2) / gc_corr_factor

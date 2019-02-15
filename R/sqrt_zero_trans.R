ed11a7066fee51a10ef0965ea2dd9a7d720b973b
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-02-15 16:32:54 +0100

    remove trans object; now in mcanouil/ggcoeos
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- R/sqrt_zero_trans.R
+++
@@ -1,9 +0,0 @@
-sqrt_zero_trans <- function() {
-  require(scales)
-  scales::trans_new(
-    name = "sqrt_zero",
-    transform = base::sqrt,
-    inverse = function(x) ifelse(x==0, 0, x^2),
-    domain = c(0, Inf)
-  )
-}

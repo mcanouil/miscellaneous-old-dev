dd739cabd5f4da03ef3897c82c834ac8f99cc431
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-03-21 17:36:09 +0100

    update with authors, affiliations, auto archive, auto tag

--- make_project/default_script.sh
+++ make_project/default_script.sh
@@ -1,19 +1,50 @@
 #!/bin/sh
 
 R --slave --silent <<RSCRIPT
-project_directory <- '~/PRJCT'
-report_directory <- paste0(project_directory, '/Report')
+user <- "Mickaël Canouil"
+email <- "mickael.canouil@cnrs.fr"
+# fexsend <- "/fexsend"
 
-dir.create(path = report_directory, recursive = TRUE, showWarnings = FALSE, mode = '0777')
+project_directory <- "~/PRJCT"
+working_directory <- gsub("/PROJECT/", "/DATATMP/", project_directory)
+
+script_name <- "00-main"
 
 rmarkdown::render(
-  input = paste0(project_directory, '/Scripts/00-main.Rmd'), 
-  output_file = '00-main.html', 
-  output_format = 'html_document', 
-  output_dir = report_directory,
+  input = paste0(project_directory, "/Scripts/", script_name, ".Rmd"), 
+  output_file = paste0(script_name, ".html"), 
+  # output_format = "html_document2", 
+  output_dir = paste(working_directory, script_name, sep = "/"),
   encoding = 'UTF-8'
 )
-RSCRIPT
 
-git -C ~/PRJCT/Report add 00-main.html
-git -C ~/PRJCT/Report commit -a 00-main.html -m 'Update report PRJCT'
+system(
+  command = paste(
+    "tar zcvf", 
+    paste0(paste(working_directory, script_name, sep = "/"), "/", Sys.Date(), "_", script_name, ".tar.gz"), 
+    "-C", paste(working_directory, script_name, sep = "/"), 
+    paste0(script_name, ".html")
+  ),
+  intern = TRUE
+)
+
+# fex_out <- system(
+#   command = paste(
+#     fexsend,
+#     paste0(output_directory, "/",  Sys.Date(), "_", script_name, ".tar.gz"),
+#     email
+#   ),
+#   intern = TRUE
+# )
+
+git <- paste0('git -c "user.name=', user, '" -c "user.email=', email, '" -C ', project_directory)
+system(paste(git, "add", paste0("Scripts/", script_name, ".Rmd")))
+system(paste(
+  git, "commit", paste0("Scripts/analyses/", script_name, ".Rmd"),
+  paste0('-m "Analysis done ', as.Date(Sys.Date(), "%Y%m%d"), '"')
+))
+system(paste(
+  git, "tag -a", script_name,
+  paste0("-m\"", "Analysis done ", as.Date(Sys.Date(), "%Y%m%d"), "\"")
+))
+RSCRIPT

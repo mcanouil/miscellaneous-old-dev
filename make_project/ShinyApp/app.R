73e7916addf4dc45fdee7fcb3851e4f6cb95bb40
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-08-01 12:58:28 +0200

    reindent
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- make_project/ShinyApp/app.R
+++ make_project/ShinyApp/app.R
@@ -2,7 +2,7 @@
 # Name - app.R
 # Desc - Shiny App for
 # Version - 1.0.0
-# Author - Mickael Canouil
+# Author - Mickaël Canouil
 # Source code -
 #---------------------------------------------------------------------------------
 rm(list = ls())
@@ -21,281 +21,273 @@ library(xlsx)
 library(lme4)
 library(tidyr)
 
-myPalettes <- rbind(
-    c(Name = "dodgerblue",  Hex = "#1E90FF", RGB = "rgb(30/255, 144/255, 255/255)"),
-    c(Name = "firebrick2",  Hex = "#EE2C2C", RGB = "rgb(238/255, 44/255, 44/255)"),
-    c(Name = "springgreen3",  Hex = "#008B45", RGB = "rgb(0/255, 139/255, 69/255)"),
-    c(Name = "maroon2",  Hex = "#EE30A7", RGB = "rgb(238/255, 48/255, 167/255)"),
-    c(Name = "goldenrod2",  Hex = "#EEB422", RGB = "rgb(238/255, 180/255, 34/255)"),
-    c(Name = "deepskyblue",  Hex = "#00BFFF", RGB = "rgb(0/255, 191/255, 255/255)"),
-    c(Name = "mediumpurple1",  Hex = "#AB82FF", RGB = "rgb(171/255, 130/255, 255/255)"),
-    c(Name = "tan1",  Hex = "#FFA54F", RGB = "rgb(255/255, 165/255, 79/255)")
-)
-myPalette <- myPalettes[, "Name"]
-
 ui <- dashboardPage(
-    dashboardHeader(
-        title = HTML("PRJCT"),
-        dropdownMenuOutput("messageMenu")
+  dashboardHeader(
+    title = HTML("PRJCT"),
+    dropdownMenuOutput("messageMenu")
+  ),
+  dashboardSidebar(
+    sidebarUserPanel(
+      name = a(tags$i(style = "color:#1995dc", icon("envelope", lib = "glyphicon")), "Mickaël Canouil", href = "mailto:mickael.canouil@cnrs.fr"),
+      subtitle = tags$span(style = "color:#1995dc", "(Biostatistician)")
     ),
-    dashboardSidebar(
-        sidebarUserPanel(
-            name = a(tags$i(style = "color:#1995dc", icon("envelope", lib = "glyphicon")), "Mickaël Canouil", href = 'mailto:mickael.canouil@cnrs.fr'),
-            subtitle = tags$span(style = "color:#1995dc", "(Biostatistician)")
-        ),
-        hr(),
-        sidebarMenu(
-            tags$p(style = "text-align:center",
-                actionButton(
-                    inputId = "reset",
-                    label = HTML(paste(
-                        icon("warning-sign", lib = "glyphicon"),
-                        "Reset Database",
-                        icon("warning-sign", lib = "glyphicon")
-                    )),
-                    width = "85%",
-                    style = "color:#EE2C2C"
-                )
-            ),
-            hr(),
-            menuItem(
-                text = "Base Tab",
-                tabName = "BaseTab",
-                icon = tags$i(style = "color:#1995dc", icon("import", lib = "glyphicon"))
-            ),
-            hr(),
-            HTML(
-                '<li class="false">
-                    <a aria-expanded="true" data-toggle="tab" data-value="Slider">
-                        <i style="color:#1995dc"><i class="glyphicon glyphicon-cog"></i></i>
-                        <span>Slider</span>
-                    </a>
-                </li>'
-            ),
-            sliderInput(
-                inputId = "Slider",
-                label = NULL,
-                min = 1,
-                max = 3,
-                value = 2,
-                step = 0.25
-            ),
-            hr(),
-            menuItem(
-                text = "Quality Control",
-                menuSubItem(
-                    text = "First Tab",
-                    tabName = "FirstTab"
-                ),
-                menuSubItem(
-                    text = "Second Tab",
-                    tabName = "SecondTab"
-                ),
-                menuSubItem(
-                    text = "Summary Tab",
-                    tabName = "SummaryTab"
-                ),
-                icon = tags$i(style = "color:#1995dc", icon("search", lib = "glyphicon"))
-            ),
-            menuItem(
-                text = "Analysis",
-                menuSubItem(
-                    text = "First Analysis",
-                    tabName = "FirstAnalysis"
-                ),
-                menuSubItem(
-                    text = "Second Analysis",
-                    tabName = "SecondAnalysis"
-                ),
-                icon = tags$i(style = "color:#1995dc", icon("tasks", lib = "glyphicon"))
-            ),
-            hr()
+    hr(),
+    sidebarMenu(
+      tags$p(
+        style = "text-align:center",
+        actionButton(
+          inputId = "reset",
+          label = HTML(paste(
+            icon("warning-sign", lib = "glyphicon"),
+            "Reset Database",
+            icon("warning-sign", lib = "glyphicon")
+          )),
+          width = "85%",
+          style = "color:#EE2C2C"
         )
-    ),
-    dashboardBody(
-        tags$head(
-            tags$link(rel = "stylesheet", type = "text/css", href = "css/theme.css")
+      ),
+      hr(),
+      menuItem(
+        text = "Base Tab",
+        tabName = "BaseTab",
+        icon = tags$i(style = "color:#1995dc", icon("import", lib = "glyphicon"))
+      ),
+      hr(),
+      HTML(
+        '<li class="false">
+          <a aria-expanded="true" data-toggle="tab" data-value="Slider">
+            <i style="color:#1995dc"><i class="glyphicon glyphicon-cog"></i></i>
+            <span>Slider</span>
+          </a>
+       </li>'
+      ),
+      sliderInput(
+        inputId = "Slider",
+        label = NULL,
+        min = 1,
+        max = 3,
+        value = 2,
+        step = 0.25
+      ),
+      hr(),
+      menuItem(
+        text = "Quality Control",
+        menuSubItem(
+          text = "First Tab",
+          tabName = "FirstTab"
         ),
-        tabItems(
-            tabItem(tabName = "BaseTab",
-                fluidRow(
-                    valueBoxOutput("messageBox"),
-                    valueBoxOutput("progressBox"),
-                    valueBoxOutput("clearBox")
-                ),
-                fluidRow(
-                    box(
-                        width = 12,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "info"
-                    )
-                ),
-                fluidRow(
-                    box(
-                        width = 12,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "primary"
-                    )
-                )
-            ),
-            tabItem(tabName = "FirstTab",
-                fluidRow(
-                    box(
-                        width = 6,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "primary"
-                    ),
-                    box(
-                        width = 6,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "primary"
-                    )
-                )
-            ),
-            tabItem(tabName = "SecondTab",
-                fluidRow(
-                    box(
-                        width = 6,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "primary"
-                    ),
-                    box(
-                        width = 6,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "primary"
-                    )
-                ),
-                fluidRow(
-                    box(
-                        width = 12,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "primary"
-                    )
-                )
-            ),
-            tabItem(tabName = "SummaryTab",
-                fluidRow(
-                    box(
-                        width = 12,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "primary"
-                    )
-                )
-            ),
-            tabItem(tabName = "FirstAnalysis",
-                fluidRow(
-                    box(
-                        width = 12,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "info"
-                    )
-                ),
-                fluidRow(
-                    box(
-                        width = 6,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "primary"
-                    ),
-                    box(
-                        width = 6,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "primary"
-                    )
-                )
-            ),
-            tabItem(tabName = "SecondAnalysis",
-                fluidRow(
-                    box(
-                        "",
-                        width = 12,
-                        collapsible = FALSE,
-                        title = "Box",
-                        solidHeader = TRUE,
-                        status = "info"
-                    )
-                )
-            )
-        )
+        menuSubItem(
+          text = "Second Tab",
+          tabName = "SecondTab"
+        ),
+        menuSubItem(
+          text = "Summary Tab",
+          tabName = "SummaryTab"
+        ),
+        icon = tags$i(style = "color:#1995dc", icon("search", lib = "glyphicon"))
+      ),
+      menuItem(
+        text = "Analysis",
+        menuSubItem(
+          text = "First Analysis",
+          tabName = "FirstAnalysis"
+        ),
+        menuSubItem(
+          text = "Second Analysis",
+          tabName = "SecondAnalysis"
+        ),
+        icon = tags$i(style = "color:#1995dc", icon("tasks", lib = "glyphicon"))
+      ),
+      hr()
     )
+  ),
+  dashboardBody(
+    tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "css/theme.css")),
+    tabItems(
+      tabItem(
+        tabName = "BaseTab",
+        fluidRow(
+          valueBoxOutput("messageBox"),
+          valueBoxOutput("progressBox"),
+          valueBoxOutput("clearBox")
+        ),
+        fluidRow(
+          box(
+            width = 12,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "info"
+          )
+        ),
+        fluidRow(
+          box(
+            width = 12,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "primary"
+          )
+        )
+      ),
+      tabItem(
+        tabName = "FirstTab",
+        fluidRow(
+          box(
+            width = 6,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "primary"
+          ),
+          box(
+            width = 6,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "primary"
+          )
+        )
+      ),
+      tabItem(
+        tabName = "SecondTab",
+        fluidRow(
+          box(
+            width = 6,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "primary"
+          ),
+          box(
+            width = 6,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "primary"
+          )
+        ),
+        fluidRow(
+          box(
+            width = 12,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "primary"
+          )
+        )
+      ),
+      tabItem(
+        tabName = "SummaryTab",
+        fluidRow(
+          box(
+            width = 12,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "primary"
+          )
+        )
+      ),
+      tabItem(
+        tabName = "FirstAnalysis",
+        fluidRow(
+          box(
+            width = 12,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "info"
+          )
+        ),
+        fluidRow(
+          box(
+            width = 6,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "primary"
+          ),
+          box(
+            width = 6,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "primary"
+          )
+        )
+      ),
+      tabItem(
+        tabName = "SecondAnalysis",
+        fluidRow(
+          box(
+            "",
+            width = 12,
+            collapsible = FALSE,
+            title = "Box",
+            solidHeader = TRUE,
+            status = "info"
+          )
+        )
+      )
+    )
+  )
 )
 
-server <- function (input, output) {
-    observeEvent(input$reset, {
-        # file.remove("")
-    })
+server <- function(input, output) {
+  observeEvent(input$reset, {
+    # file.remove("")
+  })
 
-    # output$ <- renderDataTable({
-        # withProgress(message = "Loading...", value = NULL, {
-        # })
-    # }, options = list(searching = FALSE))
+  # output$ <- renderDataTable({
+  # withProgress(message = "Loading...", value = NULL, {
+  # })
+  # }, options = list(searching = FALSE))
 
-    output$messageMenu <- renderMenu({
-        dropdownMenu(
-            type = "notifications",
-            notificationItem(
-                text = paste("Data computed on", format(Sys.Date(), "%Y-%d-%m"), "!"),
-                icon = icon("exclamation-sign", lib = "glyphicon"),
-                status = "info"
-            )
-        )
-    })
+  output$messageMenu <- renderMenu({
+    dropdownMenu(
+      type = "notifications",
+      notificationItem(
+        text = paste("Data computed on", format(Sys.Date(), "%Y-%d-%m"), "!"),
+        icon = icon("exclamation-sign", lib = "glyphicon"),
+        status = "info"
+      )
+    )
+  })
 
-    output$messageBox <- renderValueBox({
-        valueBox(
-            value = 0,
-            subtitle = "Message",
-            icon = icon("edit", lib = "glyphicon"),
-            color = "blue"
-        )
-    })
+  output$messageBox <- renderValueBox({
+    valueBox(
+      value = 0,
+      subtitle = "Message",
+      icon = icon("edit", lib = "glyphicon"),
+      color = "blue"
+    )
+  })
 
-    output$progressBox <- renderValueBox({
-        valueBox(
-            value = 0,
-            subtitle = "Progess",
-            icon = icon("share", lib = "glyphicon"),
-            color = "maroon"
-        )
-    })
+  output$progressBox <- renderValueBox({
+    valueBox(
+      value = 0,
+      subtitle = "Progess",
+      icon = icon("share", lib = "glyphicon"),
+      color = "maroon"
+    )
+  })
 
-    computeQC <- reactive({
-        withProgress(message = "Loading...", value = NULL, {
-        })
+  computeQC <- reactive({
+    withProgress(message = "Loading...", value = NULL, {
     })
-    output$QC_plot1 <- renderPlot({
-    })
+  })
+  output$QC_plot1 <- renderPlot({
+  })
 
-    output$clearBox <- renderValueBox({
-        valueBox(
-            value = 0,
-            subtitle = "Clear",
-            icon = icon("check", lib = "glyphicon"),
-            color = "green"
-        )
-    })
-
+  output$clearBox <- renderValueBox({
+    valueBox(
+      value = 0,
+      subtitle = "Clear",
+      icon = icon("check", lib = "glyphicon"),
+      color = "green"
+    )
+  })
 }
 
 shinyApp(ui, server)

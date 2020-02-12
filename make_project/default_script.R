6c2a4d52f1e00c61b9b70645385366c60d0b73bb
Mickaël Canouil <mickael.canouil@cnrs.fr>
2020-02-12 14:37:34 +0100

    use here package

--- make_project/default_script.R
+++ make_project/default_script.R
@@ -1,11 +1,14 @@
 options(stringsAsFactors = FALSE)
 
-script_name <- "default_script"
-project_directory <- "~/PRJCT"
-working_directory <- paste0(project_directory, "/Data")
-output_directory <- paste(working_directory, script_name, sep = "/")
+### Load packages
+library(here)
+library(tidyverse)
 
-dir.create(path = output_directory, recursive = TRUE, showWarnings = FALSE, mode = "0777")
+
+### Working directory
+
+script_name <- "default_script"
+dir.create(path = here("Data", script_name), recursive = TRUE, showWarnings = FALSE, mode = "0777")
 
 
 ### Params
@@ -14,32 +17,28 @@ user <- "Mickaël Canouil"
 email <- "mickael.canouil@cnrs.fr"
 
 
-### Load packages
-library(tidyverse)
-
-
 ### Define theme
 theme_set(theme_light(base_size = 12))
 
 
 ### Analyses
 rmarkdown::render(
-  input = paste0(project_directory, "/Scripts/", script_name, ".Rmd"), 
+  input = here("Scripts", paste0(script_name, ".Rmd")), 
   output_file = paste0(script_name, ".html"), 
-  output_dir = paste(working_directory, script_name, sep = "/"),
+  output_dir = here("Data", script_name),
   encoding = "UTF-8", 
   params = list(
     script_name = script_name,
-    project_directory = project_directory,
-    working_directory = working_directory
+    project_directory = here(),
+    working_directory = here("Data")
   )
 )
 
 
 ### Output
 archive_name <- file.path(
-  output_directory,
-  paste0(format(Sys.Date(), format = "%Y%m%d"), "_", basename(project_directory), ".tar.gz")
+  here("Data", script_name),
+  paste0(format(Sys.Date(), format = "%Y%m%d"), "_", basename(here()), ".tar.gz")
 )
-tar(tarfile = archive_name, files = paste0(c(paste0(script_name, ".html")), ".html"), compression = "gzip")
+tar(tarfile = archive_name, files = paste0(script_name, ".html"), compression = "gzip")
 fex_out <- system(paste(fexsend, archive_name, email), intern = TRUE)
